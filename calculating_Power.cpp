8f8fbfae69060faa26b9194b5fdad85bba56cdf9
Sia13 <68543317+Sia13@users.noreply.github.com>
2021-09-23 17:58:40 +0530

    Add files via upload

---
+++ calculating_Power.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int power, result_int;
+    double base, result = 1;
+    cout<<"Enter base and exponent respectively: ";
+    cin>>base>>power;
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(3);
+    if (power > 0)
+    {
+        while( power != 0)
+        {
+            result *= base;
+            --power;
+        }
+        // result_int = static_cast<int>(result);
+        cout<<"Answer = "<<result<<endl;
+    }
+    
+    else
+    {
+        while( power!=0 )
+        {
+            result *= base;
+            power++;
+        }
+        cout<<"Answer = 1/"<<result;
+    }
+    
+    return 0;
+}
